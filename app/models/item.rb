fa178bf8aac3208b8df4969bc836d2c8c0fa40b2
Kokiy8 <K8o2u5k7iko@gmail.com>
2022-01-07 13:25:33 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :days_to
+  belongs_to :prefectures
+
   has_one_attached :image
 end
